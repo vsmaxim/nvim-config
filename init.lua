a36e767a3a55bcb687268e0a023371e45b200f6f
maxim <vsmaxim8@gmail.com>
2023-02-25 10:23:24 +0100

    Sync settings (last time)

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require "config/plugins"
 require "config/completion"
 require "config/lsp"
 require "config/airline_powerline"
-require "config/key_bindings.lua"
+require "config/key_bindings"
 
 require'lspconfig'.clojure_lsp.setup{}
 
